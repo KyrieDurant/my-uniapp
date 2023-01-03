a5bdddb5c201083356db1dd2ec0799c0f5a83288
KyrieDurant <1968415571@qq.com>
2023-01-03 17:37:04 +0800

    修改草稿内容

--- pages/caogao/caogao.vue
+++ pages/caogao/caogao.vue
@@ -1,38 +1,50 @@
-<!-- 本示例未包含完整css，获取外链css请参考上文，在hello uni-app项目中查看 -->
 <template>
-    <view>
-        <view class="uni-padding-wrap uni-common-mt">
-			<view class="uni-title">设置step</view>
-			<view>
-				<slider value="60" @change="sliderChange" step="5" />
-			</view>
-			
-			<view class="uni-title">显示当前value</view>
-			<view>
-				<slider value="50" @change="sliderChange" show-value />
-			</view>
-            
-			<view class="uni-title">设置最小/最大值</view>
-			<view>
-				<slider value="100" @change="sliderChange" min="50" max="200" show-value />
-			</view>
-			
-			<view class="uni-title">不同颜色和大小的滑块</view>
-			<view>
-				<slider value="50" @change="sliderChange" activeColor="#FFCC33" backgroundColor="#000000" block-color="#8A6DE9" block-size="20" />
-			</view>
-        </view>
-    </view>
+	<view class="content">
+		<view class="">
+			第一部分
+		</view>
+		<view class="">
+			第二部分
+		</view>
+		<view class="">
+			第三部分
+		</view>
+	</view>
 </template>
+
 <script>
-export default {
-    data() {
-        return {}
-    },
-    methods: {
-        sliderChange(e) {
-            console.log('value 发生变化：' + e.detail.value)
-        }
-    }
-}
+	
+	export default {
+		data(){
+			return {
+				
+			}
+		},
+		components: {
+			oneBox,
+		},
+		onLoad() {
+			
+		},
+		methods:{
+			chaNum(e){
+				console.log('拿到子组件传过来的', e);
+				// this.childUp = e;
+			},
+		},
+	}
 </script>
+
+<style scoped>
+page{
+	width: 100%;
+	height: 100%;
+}
+.content{
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+}
+
+</style>
